9fffebfe1e5ae02ff1bfc7fa45bd45e26fc322ca
gourgiotisw <wgourgiotis@gmail.com>
2024-05-14 15:22:31 -0400

    Create initials.py

---
+++ Hello World/Block Letters/initials.py
@@ -0,0 +1,17 @@
+# William Gourgiotis
+# I like mowing over tennis balls. 
+
+print("W       W       W")
+print(" W     W W     W")
+print("  W   W   W   W")
+print("   W W     W W")
+print("    W       W")
+print("""
+      """)
+print("  GGGGG  ")
+print(" G     G ")
+print("G        ")
+print("G   GGGG ")
+print("G      G ")
+print(" G     G ")
+print("  GGGGG  ")
